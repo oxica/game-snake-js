cf16b293d6034eff6254a61a8ebe7768dc095f3c
Oksana <kskaplun@gmail.com>
2022-03-25 11:55:25 +0200

    генерируем игровое поле

---
+++ js/board.js
@@ -0,0 +1,39 @@
+game.board = {
+  game: game,
+  size: 15,
+  cells: [],
+
+  create() {
+    this.createCells();
+  },
+
+  createCells() {
+    for (let row = 0; row < this.size; row++) {
+      for (let col = 0; col < this.size; col++) {
+        let cell = this.createCell(row, col);
+        this.cells.push(cell);
+      }
+    }
+  },
+
+  createCell(row, col) {
+    let cellSize = this.game.sprites.cell.width + 1;
+
+    let offsetX = (this.game.width - this.size * cellSize) / 2;
+    let offsetY = (this.game.height - this.size * cellSize) / 2;
+
+    let cell = {
+      row: row,
+      col: col,
+      x: offsetX + cellSize * col,
+      y: offsetY + cellSize * row,
+    };
+    return cell;
+  },
+
+  render() {
+    this.cells.forEach((cell) => {
+      this.game.ctx.drawImage(this.game.sprites.cell, cell.x, cell.y);
+    });
+  },
+};
--- js/game.js
+++ js/game.js
@@ -1,23 +1,29 @@
 let game = {
   canvas: null,
   ctx: null,
+  board: null,
+  width: 640,
+  height: 360,
   sprites: {
     background: null,
     cell: null,
   },
 
   start() {
-    this.canvas = document.getElementById("mycanvas");
-    this.ctx = this.canvas.getContext("2d");
-
+    this.init();
     this.preload(() => {
       this.run();
     });
   },
 
+  init() {
+    this.canvas = document.getElementById("mycanvas");
+    this.ctx = this.canvas.getContext("2d");
+  },
+
   preload(callback) {
     let loaded = 0;
-    let required = 2;
+    let required = Object.keys(this.sprites).length;
     let onAssetLoad = () => {
       ++loaded;
       if (loaded >= required) {
@@ -32,9 +38,11 @@ let game = {
   },
 
   run() {
+    this.board.create();
+
     window.requestAnimationFrame(() => {
       this.ctx.drawImage(this.sprites.background, 0, 0);
-      this.ctx.drawImage(this.sprites.cell, 320, 180);
+      this.board.render();
     });
   },
 };
