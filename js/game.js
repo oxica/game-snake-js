b00ad2a13f69b8a3e6a98108423601a005f6e90c
Oksana <kskaplun@gmail.com>
2022-03-25 10:56:32 +0200

    preloader для спрайтов

--- js/game.js
+++ js/game.js
@@ -1,18 +1,42 @@
 let game = {
-  start: function () {
-    let canvas = document.getElementById("mycanvas");
-        let ctx = canvas.getContext("2d");
-        
-    let background = new Image();
-    background.src = "img/background.png";
-        background.addEventListener("load", () => {
-        window.requestAnimationFrame(() => {
-            ctx.drawImage(background, 0, 0);
-        });
-        });
-  }
+  canvas: null,
+  ctx: null,
+  sprites: {
+    background: null,
+    cell: null,
+  },
+
+  start() {
+    this.canvas = document.getElementById("mycanvas");
+    this.ctx = this.canvas.getContext("2d");
+
+    this.preload(() => {
+      this.run();
+    });
+  },
+
+  preload(callback) {
+    let loaded = 0;
+    let required = 2;
+    let onAssetLoad = () => {
+      ++loaded;
+      if (loaded >= required) {
+        callback();
+      }
+    };
+    for (let key in this.sprites) {
+      this.sprites[key] = new Image();
+      this.sprites[key].src = "img/" + key + ".png";
+      this.sprites[key].addEventListener("load", onAssetLoad);
+    }
+  },
+
+  run() {
+    window.requestAnimationFrame(() => {
+      this.ctx.drawImage(this.sprites.background, 0, 0);
+      this.ctx.drawImage(this.sprites.cell, 320, 180);
+    });
+  },
 };
 
-
 game.start();
-
